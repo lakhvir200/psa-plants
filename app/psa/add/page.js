2b37ba490037ebf9fad3177eb820b7a0a81fcf6d
lakhvir200 <lakhvirsingh2003@gmail.com>
2025-05-28 11:38:19 +0530

    check popup v1

---
+++ app/psa/add/page.js
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+const AddEquipments = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to Add Equipments</h1>
+      <p>Please fill in the equipment details below.</p>
+    </div>
+  );
+};
+
+export default AddEquipments;
